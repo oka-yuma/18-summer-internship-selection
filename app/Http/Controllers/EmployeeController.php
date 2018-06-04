11c1f3d1d7196e0e0cb5f1d77910cc5daa94f349
千原　丈 <takeru.chihara@asia-quest.jp>
2018-06-04 20:01:58 +0900

    難易度調整

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -34,8 +34,14 @@ class EmployeeController extends Controller
         $employees = $employees->where('given_name', 'like', '%' . $request->get('given_name') . '%');
         $employees = $employees->where('family_name_kana', 'like', '%' . $request->get('family_name_kana') . '%');
         $employees = $employees->where('given_name_kana', 'like', '%' . $request->get('given_name_kana') . '%');
+        // ↓ここから　バグ
+        // ①この処理は何を狙った処理なのか考える
+        // ②なぜバグが出てしまうのかを考える
+        // ③どうしたらそうならないかを考える
+        //  dd($request->get('department_id'));　等をして、 requestのパラメータに興味を持とう
         $employees = $employees->where('divisions.department_id', $request->get('department_id'));
         $employees = $employees->where('employees.division_id',  $request->get('division_id'));
+        // ↑ここまで　バグ
 
         // ソート
         $employees = $employees->orderBy($request->get('sort_key', 'id'),$request->get('sort_order', 'asc'));
